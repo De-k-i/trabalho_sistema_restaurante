a4e7d36ede9d94ff5a938616cf35c3fd1c9dc72a
Gabriel Deeke Schwarz <de.eke@hotmail.com>
2026-07-24 18:32:10 -0300

    Refactor menu and product registration logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +13,7 @@ namespace Trabaio
             List<ItemCardapio> listaCardapio = new List<ItemCardapio>();
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine("***** MENU *****");
                 Console.WriteLine("1) Cadastrar Item");
                 Console.WriteLine("2) Listar Cardápio");
@@ -20,14 +21,16 @@ namespace Trabaio
                 Console.WriteLine("4) Pausar / Reativar Vendas");
                 Console.WriteLine("5) Remover Itens");
                 Console.WriteLine("6) Sair");
+                Console.Write("Opção: ");
                 if (!int.TryParse(Console.ReadLine(), out int opcao)) { Console.WriteLine("Entrada inválida."); continue; }
                 if (opcao == 6) break;
                 switch (opcao)
                 {
                     case 1:
-                        CadastrarProduto();
+                        CadastrarProduto(listaCardapio);
                         break;
                     case 2:
+                        ListarProdutos(listaCardapio);
                         break;
                     case 3:
                         break;
@@ -40,27 +43,45 @@ namespace Trabaio
                 }
             }
         }
-        static void CadastrarProduto()
+        static void CadastrarProduto(List<ItemCardapio> lista)
         {
+            Console.Clear();
             Console.WriteLine("Informe o nome do produto: ");
             string nome = Console.ReadLine();
             Console.WriteLine("Selecione a categoria do produto: ");
             Categoria categoria;
             while (true)
             {
-                Console.WriteLine("[1 - Hambúrguer\t2 - Acompanhamento\t3 - Adicional\t4 - Sobremesa\t5 - Bebida]");
-                if (int.TryParse(Console.ReadLine(), out int opcao) && Enum.IsDefined(typeof(Categoria),opcao)) {
+                Console.WriteLine("1 - Hambúrguer\n2 - Acompanhamento\n3 - Adicional\n4 - Sobremesa\n5 - Bebida");
+                Console.Write("Opção: ");
+                if (int.TryParse(Console.ReadLine(), out int opcao) && Enum.IsDefined(typeof(Categoria), opcao))
+                {
                     categoria = (Categoria)opcao;
                     break;
                 }
                 Console.WriteLine("Opção inválida.");
             }
-            Console.WriteLine("Informe o preço do produto: ");
-            while (!decimal.TryParse(Console.ReadLine(), out decimal preco))
+            decimal preco;
+            while (true)
             {
+                Console.WriteLine("Informe o preço do produto: ");
+                if (decimal.TryParse(Console.ReadLine(), out preco)) break;
                 Console.WriteLine("Por favor informe um preço válido.");
             }
+            lista.Add(new ItemCardapio(lista.Count+1,nome, categoria, preco));
+            Console.WriteLine("***** PRODUTO CADASTRADO *****");
+            Console.ReadKey();
+        }
 
+        static void ListarProdutos(List<ItemCardapio> lista)
+        {
+            Console.Clear();
+            Console.WriteLine("***** CARDÁPIO *****");
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
